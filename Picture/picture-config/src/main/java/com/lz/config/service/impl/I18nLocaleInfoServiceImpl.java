04d42c80b68b21ebb0aa250359ae09a228298b53
SpringSunYY <3217778348@qq.com>
2025-02-28 22:56:59 +0800

    国际化国家信息新增修改

--- Picture/picture-config/src/main/java/com/lz/config/service/impl/I18nLocaleInfoServiceImpl.java
+++ Picture/picture-config/src/main/java/com/lz/config/service/impl/I18nLocaleInfoServiceImpl.java
@@ -5,9 +5,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.stream.Collectors;
+
+import com.lz.common.exception.sql.SQLDuplicateKeyException;
+import com.lz.common.utils.SecurityUtils;
 import com.lz.common.utils.StringUtils;
 import com.lz.common.utils.DateUtils;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -23,13 +27,14 @@ import com.lz.config.model.vo.i18nLocaleInfo.I18nLocaleInfoVo;
  * @author ruoyi
  * @date 2025-02-28
  */
+@Slf4j
 @Service
-public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper, I18nLocaleInfo> implements II18nLocaleInfoService
-{
+public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper, I18nLocaleInfo> implements II18nLocaleInfoService {
     @Resource
     private I18nLocaleInfoMapper i18nLocaleInfoMapper;
 
     //region mybatis代码
+
     /**
      * 查询国际化国家
      *
@@ -37,8 +42,7 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 国际化国家
      */
     @Override
-    public I18nLocaleInfo selectI18nLocaleInfoByLocaleId(Long localeId)
-    {
+    public I18nLocaleInfo selectI18nLocaleInfoByLocaleId(Long localeId) {
         return i18nLocaleInfoMapper.selectI18nLocaleInfoByLocaleId(localeId);
     }
 
@@ -49,8 +53,7 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 国际化国家
      */
     @Override
-    public List<I18nLocaleInfo> selectI18nLocaleInfoList(I18nLocaleInfo i18nLocaleInfo)
-    {
+    public List<I18nLocaleInfo> selectI18nLocaleInfoList(I18nLocaleInfo i18nLocaleInfo) {
         return i18nLocaleInfoMapper.selectI18nLocaleInfoList(i18nLocaleInfo);
     }
 
@@ -61,10 +64,15 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 结果
      */
     @Override
-    public int insertI18nLocaleInfo(I18nLocaleInfo i18nLocaleInfo)
-    {
-                i18nLocaleInfo.setCreateTime(DateUtils.getNowDate());
+    public int insertI18nLocaleInfo(I18nLocaleInfo i18nLocaleInfo) {
+        i18nLocaleInfo.setCreateBy(SecurityUtils.getUsername());
+        i18nLocaleInfo.setCreateTime(DateUtils.getNowDate());
+        try {
             return i18nLocaleInfoMapper.insertI18nLocaleInfo(i18nLocaleInfo);
+        } catch (Exception e) {
+            log.error("插入国际化国家失败：{}", e.getMessage(), e.getCause());
+            throw new SQLDuplicateKeyException(e.getMessage(), e.getCause());
+        }
     }
 
     /**
@@ -74,10 +82,15 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 结果
      */
     @Override
-    public int updateI18nLocaleInfo(I18nLocaleInfo i18nLocaleInfo)
-    {
-                i18nLocaleInfo.setUpdateTime(DateUtils.getNowDate());
-        return i18nLocaleInfoMapper.updateI18nLocaleInfo(i18nLocaleInfo);
+    public int updateI18nLocaleInfo(I18nLocaleInfo i18nLocaleInfo) {
+        i18nLocaleInfo.setUpdateBy(SecurityUtils.getUsername());
+        i18nLocaleInfo.setUpdateTime(DateUtils.getNowDate());
+        try {
+            return i18nLocaleInfoMapper.updateI18nLocaleInfo(i18nLocaleInfo);
+        } catch (Exception e) {
+            log.error("修改国际化国家失败：{}", e.getMessage(), e.getCause());
+            throw new SQLDuplicateKeyException(e.getMessage(), e.getCause());
+        }
     }
 
     /**
@@ -87,8 +100,7 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 结果
      */
     @Override
-    public int deleteI18nLocaleInfoByLocaleIds(Long[] localeIds)
-    {
+    public int deleteI18nLocaleInfoByLocaleIds(Long[] localeIds) {
         return i18nLocaleInfoMapper.deleteI18nLocaleInfoByLocaleIds(localeIds);
     }
 
@@ -99,39 +111,39 @@ public class I18nLocaleInfoServiceImpl extends ServiceImpl<I18nLocaleInfoMapper,
      * @return 结果
      */
     @Override
-    public int deleteI18nLocaleInfoByLocaleId(Long localeId)
-    {
+    public int deleteI18nLocaleInfoByLocaleId(Long localeId) {
         return i18nLocaleInfoMapper.deleteI18nLocaleInfoByLocaleId(localeId);
     }
+
     //endregion
     @Override
-    public QueryWrapper<I18nLocaleInfo> getQueryWrapper(I18nLocaleInfoQuery i18nLocaleInfoQuery){
+    public QueryWrapper<I18nLocaleInfo> getQueryWrapper(I18nLocaleInfoQuery i18nLocaleInfoQuery) {
         QueryWrapper<I18nLocaleInfo> queryWrapper = new QueryWrapper<>();
         //如果不使用params可以删除
         Map<String, Object> params = i18nLocaleInfoQuery.getParams();
         if (StringUtils.isNull(params)) {
             params = new HashMap<>();
         }
-                String localeName = i18nLocaleInfoQuery.getLocaleName();
-                    queryWrapper.like(StringUtils.isNotEmpty(localeName) ,"locale_name",localeName);
+        String localeName = i18nLocaleInfoQuery.getLocaleName();
+        queryWrapper.like(StringUtils.isNotEmpty(localeName), "locale_name", localeName);
 
-                String locale = i18nLocaleInfoQuery.getLocale();
-                    queryWrapper.eq(StringUtils.isNotEmpty(locale) ,"locale",locale);
+        String locale = i18nLocaleInfoQuery.getLocale();
+        queryWrapper.eq(StringUtils.isNotEmpty(locale), "locale", locale);
 
-                String localeStatus = i18nLocaleInfoQuery.getLocaleStatus();
-                    queryWrapper.eq(StringUtils.isNotEmpty(localeStatus) ,"locale_status",localeStatus);
+        String localeStatus = i18nLocaleInfoQuery.getLocaleStatus();
+        queryWrapper.eq(StringUtils.isNotEmpty(localeStatus), "locale_status", localeStatus);
 
-                String createBy = i18nLocaleInfoQuery.getCreateBy();
-                    queryWrapper.like(StringUtils.isNotEmpty(createBy) ,"create_by",createBy);
+        String createBy = i18nLocaleInfoQuery.getCreateBy();
+        queryWrapper.like(StringUtils.isNotEmpty(createBy), "create_by", createBy);
 
-                Date createTime = i18nLocaleInfoQuery.getCreateTime();
-                    queryWrapper.between(StringUtils.isNotNull(params.get("beginCreateTime"))&&StringUtils.isNotNull(params.get("endCreateTime")),"create_time",params.get("beginCreateTime"),params.get("endCreateTime"));
+        Date createTime = i18nLocaleInfoQuery.getCreateTime();
+        queryWrapper.between(StringUtils.isNotNull(params.get("beginCreateTime")) && StringUtils.isNotNull(params.get("endCreateTime")), "create_time", params.get("beginCreateTime"), params.get("endCreateTime"));
 
-                String updateBy = i18nLocaleInfoQuery.getUpdateBy();
-                    queryWrapper.like(StringUtils.isNotEmpty(updateBy) ,"update_by",updateBy);
+        String updateBy = i18nLocaleInfoQuery.getUpdateBy();
+        queryWrapper.like(StringUtils.isNotEmpty(updateBy), "update_by", updateBy);
 
-                Date updateTime = i18nLocaleInfoQuery.getUpdateTime();
-                    queryWrapper.between(StringUtils.isNotNull(params.get("beginUpdateTime"))&&StringUtils.isNotNull(params.get("endUpdateTime")),"update_time",params.get("beginUpdateTime"),params.get("endUpdateTime"));
+        Date updateTime = i18nLocaleInfoQuery.getUpdateTime();
+        queryWrapper.between(StringUtils.isNotNull(params.get("beginUpdateTime")) && StringUtils.isNotNull(params.get("endUpdateTime")), "update_time", params.get("beginUpdateTime"), params.get("endUpdateTime"));
 
         return queryWrapper;
     }
