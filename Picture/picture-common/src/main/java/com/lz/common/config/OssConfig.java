1ca2cc11ee57bc7aa73b6409b2bb98a629117d25
SpringSunYY <3217778348@qq.com>
2025-03-27 10:05:12 +0800

    oss环境

---
+++ Picture/picture-common/src/main/java/com/lz/common/config/OssConfig.java
@@ -0,0 +1,33 @@
+package com.lz.common.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Project: Picture
+ * Package: com.lz.common.config
+ * Author: YY
+ * CreateTime: 2025-03-27  09:38
+ * Description: OssConfig
+ * Version: 1.0
+ */
+@Data
+@Configuration
+@PropertySource("classpath:application-config.properties")
+public class OssConfig {
+    @Value("${aliYun.accessKeyId}")
+    private String accessKeyId;
+    @Value("${aliYun.accessKeySecret}")
+    private String accessKeySecret;
+    @Value("${aliYun.bucket}")
+    private String bucket;
+    @Value("${aliYun.dir}")
+    private String dir;
+    @Value("${aliYun.endpoint}")
+    private String endpoint;
+    @Value("${aliYun.region}")
+    private String region;
+}
