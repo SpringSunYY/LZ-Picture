27d18bdc509dc5da4bc73a6256ad50dec733f457
SpringSunYY <3217778348@qq.com>
2025-09-06 23:42:30 +0800

    管理端前端：zoom折线图

--- Picture-back-ui/src/views/user/statistics/index.vue
+++ Picture-back-ui/src/views/user/statistics/index.vue
@@ -2,12 +2,14 @@
   <div>
     <PieCharts/>
     <BarRankingCharts/>
+    <LineZoomCharts/>
   </div>
 </template>
 <script setup>
 
 import PieCharts from "@/components/echarts/PieCharts";
 import BarRankingCharts from "@/components/echarts/BarRankingCharts";
+import LineZoomCharts from "@/components/echarts/LineZoomCharts.vue";
 </script>
 <style scoped lang="scss">
 
