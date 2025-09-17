ac613a1af60e0dd6c2d53ab9c4d7e4afaf2a2171
SpringSunYY <3217778348@qq.com>
2025-09-17 18:27:08 +0800

    管理端前端：平均柱形图

--- Picture-back-ui/src/views/picture/statistics/index.vue
+++ Picture-back-ui/src/views/picture/statistics/index.vue
@@ -2,10 +2,14 @@
   <div style="height: 300px">
     <KeywordCharts/>
   </div>
+  <div style="height: 300px">
+    <BarAvgStatisics/>
+  </div>
 </template>
 <script setup lang="ts">
 
 import KeywordCharts from "@/components/Statistics/KeywordCharts.vue";
+import BarAvgStatisics from "@/components/Statistics/BarAvgStatisics.vue";
 </script>
 <style scoped lang="scss">
 
