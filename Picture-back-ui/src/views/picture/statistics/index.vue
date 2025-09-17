dcc6f8f8624c73be9f462d0ba1482fdcd26bf7d7
SpringSunYY <3217778348@qq.com>
2025-09-17 18:36:35 +0800

    管理端前端：平均柱形图改名平均折线图

--- Picture-back-ui/src/views/picture/statistics/index.vue
+++ Picture-back-ui/src/views/picture/statistics/index.vue
@@ -3,12 +3,12 @@
     <KeywordCharts/>
   </div>
   <div style="height: 300px">
-    <BarAvgStatistics/>
+    <LineAvgStatistics/>
   </div>
 </template>
 <script setup lang="ts">
 import KeywordCharts from "@/components/Statistics/KeywordCharts.vue";
-import BarAvgStatistics from "@/components/Statistics/BarAvgStatistics.vue";
+import LineAvgStatistics from "@/components/Statistics/LineAvgStatistics.vue";
 </script>
 <style scoped lang="scss">
 
