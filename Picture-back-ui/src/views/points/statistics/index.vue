d7d95dd1f325a5d4740f764b5b4821f1c1b8ef3e
SpringSunYY <3217778348@qq.com>
2025-09-22 17:37:13 +0800

    管理端前端：柱形平均轮播图

--- Picture-back-ui/src/views/points/statistics/index.vue
+++ Picture-back-ui/src/views/points/statistics/index.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <div class="points-statistics">
+    <MapCharts/>
+    <LineAvgCharts/>
+    <BarAxisRankingCharts chart-direction="right"/>
+    <BarRankingCharts/>
+    <LineZoomCharts/>
+    <PieLineCharts/>
+    <PieIntervalCharts/>
+    <BarAutoCarouselCharts/>
+  </div>
 </template>
+<script setup>
 
+import MapCharts from "@/components/Statistics/MapCharts.vue";
+import LineAvgCharts from "@/components/Statistics/LineAvgCharts.vue";
+import BarAxisRankingCharts from "@/components/Statistics/BarAxisRankingCharts.vue";
+import LineZoomCharts from "@/components/Statistics/LineZoomCharts.vue";
+import PieLineCharts from "@/components/Statistics/PieLineCharts.vue";
+import BarRankingCharts from "@/components/Statistics/BarRankingCharts.vue";
+import PieIntervalCharts from "@/components/Statistics/PieIntervalCharts.vue";
+import BarAutoCarouselCharts from "@/components/Statistics/BarAutoCarouselCharts.vue";
+</script>
 <style scoped lang="scss">
-
+.points-statistics {
+  width: 100%;
+  height: 100%;
+}
 </style>
